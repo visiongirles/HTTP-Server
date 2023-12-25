8071a2549f2e89ded1dab5b25bf037828aefcfcd
Ekaterina Sychenko <busy.sychenko@gmail.com>
2023-12-25 19:51:17 +0400

    pass 3rd stage

--- app/main.js
+++ app/main.js
@@ -5,12 +5,33 @@ console.log('Logs from your program will appear here!');
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  const answer = 'HTTP/1.1 200 OK\r\n\r\n';
-  socket.write(answer);
+  socket.on('data', (data) => {
+    const dataArray = data.toString().split(`\r\n`);
+    const url = dataArray[0].split(' ')[1];
+
+    const answer = 'HTTP/1.1 200 OK\r\n\r\n';
+    const error = `HTTP/1.1 404 Not Found\r\n\r\n`;
+
+    if (url === '/') {
+      socket.write(answer);
+    } else {
+      socket.write(error);
+    }
+  });
+
   socket.on('close', () => {
     socket.end();
     server.close();
   });
 });
+
+// server.on('connection', (socket) => {
+//   socket.on('data', (data) => {
+//     console.log(`${data.toString()}`);
+//   });
+
+//   const answer = 'HTTP/1.1 200 OK\r\n\r\n';
+//   socket.write(answer);
+// });
 
 server.listen(4221, 'localhost');
