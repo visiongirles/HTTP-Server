4b7ab13b94fdf6e97b60399cc7502d0437d6687a
Ekaterina Sychenko <busy.sychenko@gmail.com>
2023-12-27 17:23:45 +0400

    pass 7th stage

--- app/main.js
+++ app/main.js
@@ -1,12 +1,20 @@
 const net = require('net');
+const { argv } = require('node:process');
+const fs = require('node:fs/promises');
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log('Logs from your program will appear here!');
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
+  if (argv.length > 2) {
+    const directory = argv[2];
+    //O_RDWR
+  }
   socket.on('data', (data) => {
     const dataArray = data.toString().split(`\r\n`);
+    // console.log(dataArray);
+
     const [method, path, protocol] = dataArray[0].split(' ');
     const [_, userAgentInfo] = dataArray[2].split(' ');
 
@@ -14,7 +22,20 @@ const server = net.createServer((socket) => {
 
     const condition5thStage = '/user-agent';
 
-    if (path.startsWith(condition5thStage)) {
+    const condition7thStage = '/files/';
+
+    if (path.startsWith(condition7thStage)) {
+      const filename = path.split('/')[1];
+
+      const filePath = `${directory}/${filename}`;
+      fs.access(filePath, fs.constants.F_OK, (err) => {
+        if (!err) {
+          console.log(`Файл ${filePath} существует.`);
+        } else {
+          console.error(`Файл ${filePath} не существует.`);
+        }
+      });
+    } else if (path.startsWith(condition5thStage)) {
       const status = `HTTP/1.1 200 OK`;
       const headerContentType = `Content-Type: text/plain`;
       const body = userAgentInfo;
@@ -39,12 +60,12 @@ const server = net.createServer((socket) => {
         '',
         body,
       ].join('\r\n');
-      // const answer4thStage = `HTTP/1.1 200 OK\r\n${header}\r\n${randomString}`;
       socket.write(response);
     } else {
       if (path === '/') {
-        const response = `HTTP/1.1 200 OK\r\n\r\n`;
-        socket.write(response);
+        const status = `HTTP/1.1 200 OK\r\n`;
+        const header = `Content-Length: 0\r\n\r\n`;
+        socket.write(status + header);
       } else {
         const error = `HTTP/1.1 404 Not Found\r\n\r\n`;
         socket.write(error);
@@ -53,18 +74,10 @@ const server = net.createServer((socket) => {
   });
 
   socket.on('close', () => {
+    console.log('ALLO&@');
     socket.end();
     server.close();
   });
 });
-
-// server.on('connection', (socket) => {
-//   socket.on('data', (data) => {
-//     console.log(`${data.toString()}`);
-//   });
-
-//   const answer = 'HTTP/1.1 200 OK\r\n\r\n';
-//   socket.write(answer);
-// });
 
 server.listen(4221, 'localhost');
