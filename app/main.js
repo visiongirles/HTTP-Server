eaec948d918c1f760ab88ba36eb20b16bea5b2ce
Ekaterina Sychenko <busy.sychenko@gmail.com>
2023-12-29 17:02:44 +0400

    pass 7th stage

--- app/main.js
+++ app/main.js
@@ -6,15 +6,28 @@ const fs = require('node:fs');
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log('Logs from your program will appear here!');
 
-// Uncomment this to pass the first stage
+function createResponse(status, headerContentType, body, bodySize) {
+  const responseStatus = `HTTP/1.1 ${status}`;
+  const responseHeaderContentType = headerContentType`Content-Type: ${headerContentType}`;
+  const responseHeaderContentLength = `Content-Length: ${bodySize}`;
+  const responseHeaders =
+    headerContentType === ''
+      ? responseHeaderContentLength
+      : responseHeaderContentLength + '\r\n' + responseHeaderContentType;
+
+  const responseBody = body;
+  const response = [responseStatus, responseHeaders, '', responseBody].join(
+    '\r\n'
+  );
+
+  return response;
+}
+
 const server = net.createServer((socket) => {
   socket.on('data', async (data) => {
-    const dataArray = data.toString().split(`\r\n`);
-    // console.log(dataArray);
-
-    const [method, path, protocol] = dataArray[0].split(' ');
-    const [_, userAgentInfo] = dataArray[2].split(' ');
-
+    const requestArray = data.toString().split(`\r\n`);
+    const [method, path, protocol] = requestArray[0].split(' ');
+    const [_, userAgentInfo] = requestArray[2].split(' ');
     const condition4thStage = `/echo/`;
 
     const condition5thStage = '/user-agent';
@@ -22,70 +35,47 @@ const server = net.createServer((socket) => {
     const condition7thStage = '/files/';
 
     if (path.startsWith(condition7thStage)) {
-      const [execPath, execFile, flag, directory] = argv;
+      switch (method) {
+        case 'GET': {
+          const [execPath, execFile, flag, directory] = argv;
 
-      // argv: /usr/local/bin/node,/app/app/main.js,--directory,/tmp/data/codecrafters.io/http-server-tester/
-      const filename = path.substring(7);
-      const filePath = `${directory}${filename}`;
-      // try {const stats = await fs.stat(filePath);
-      //   socket.write(response)}
-      //   catch(e){}
+          // argv: /usr/local/bin/node,/app/app/main.js,--directory,/tmp/data/codecrafters.io/http-server-tester/
+          const filename = path.substring(7);
+          const filePath = `${directory}${filename}`;
 
-      try {
-        const stats = await fsPromises.stat(filePath);
-        console.log(`Stats: ${stats}`);
-        const status = `HTTP/1.1 200 OK`;
-        const headerContentType = `Content-Type: application/octet-stream`;
-        const headerContentLength = `Content-Length:${stats.size}`;
-        const response = [
-          status,
-          headerContentType,
-          headerContentLength,
-          '',
-          '',
-        ].join('\r\n');
-        socket.write(response);
+          try {
+            const stats = await fsPromises.stat(filePath);
+            console.log(`Stats: ${stats}`);
+            const status = `HTTP/1.1 200 OK`;
+            const headerContentType = `Content-Type: application/octet-stream`;
+            const headerContentLength = `Content-Length:${stats.size}`;
+            const response = [
+              status,
+              headerContentType,
+              headerContentLength,
+              '',
+              '',
+            ].join('\r\n');
+            socket.write(response);
 
-        const readableStream = fs.createReadStream(filePath);
-        // readableStream.on('error', (error) => {
-        //   return;
-        //   // socket.write(error);
-        // });
-
-        readableStream.on('data', (chunk) => {
-          console.log('In data event');
-          socket.write(chunk);
-        });
-      } catch (error) {
-        const errorResponse = `HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n`;
-        socket.write(errorResponse);
-        console.log(`Error from catch: ${error}`);
+            const readableStream = fs.createReadStream(filePath);
+            readableStream.on('data', (chunk) => {
+              console.log('In data event');
+              socket.write(chunk);
+            });
+          } catch (error) {
+            console.log(`Error from catch: ${error}`);
+            const errorResponse = `HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n`;
+            socket.write(errorResponse);
+          }
+          break;
+        }
+        case 'POST': {
+          break;
+        }
+        default:
+          break;
       }
-
-      // fs.stat(filePath, (error, stats) => {
-      //   if (!error) {
-      //     const status = `HTTP/1.1 200 OK`;
-      //     const headerContentType = `Content-Type: application/octet-stream`;
-      //     const context = fs.readFile(filePath, { encoding: 'utf8' });
-      //     const headerContentLength = `Content-Length:${stats.size}`;
-      //     const body = fs.readFileSync();
-      //     const response = [
-      //       status,
-      //       headerContentType,
-      //       headerContentLength,
-      //       '',
-      //       '',
-      //     ].join('\r\n');
-      // socket.pipe();
-      //     socket.write(response);
-      // read();
-      // console.log(`Файл ${filePath} существует.`);
-      //   } else {
-      //     const errorResponse = `HTTP/1.1 404 Not Found\r\n\r\n`;
-      //     socket.write(errorResponse);
-      //     // console.error(`Файл ${filePath} не существует.`);
-      //   }
-      // });
     } else if (path.startsWith(condition5thStage)) {
       const status = `HTTP/1.1 200 OK`;
       const headerContentType = `Content-Type: text/plain`;
@@ -114,17 +104,21 @@ const server = net.createServer((socket) => {
       socket.write(response);
     } else {
       if (path === '/') {
-        const status = `HTTP/1.1 200 OK\r\n`;
-        const header = `Content-Length: 0\r\n\r\n`;
-        socket.write(status + header);
+        const response = createResponse('200 OK', '', '', 0);
+        // const status = `HTTP/1.1 200 OK\r\n`;
+        // const header = `Content-Length: 0\r\n\r\n`;
+        // socket.write(status + header);
+        socket.write(response);
       } else {
-        const error = `HTTP/1.1 404 Not Found\r\n\r\n`;
+        // const error = `HTTP/1.1 404 Not Found\r\n\r\n`;
+        const error = createResponse('404 Not Found', '', '', 0);
         socket.write(error);
       }
     }
   });
 
   socket.on('close', () => {
+    console.log(`On close event`);
     socket.end();
     server.close();
   });
